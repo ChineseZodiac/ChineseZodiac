2e545e0992b5c3f97eb65b46d4a5f1c5251999b9
HyeranShin <hyebaragi__@naver.com>
2018-10-18 21:57:13 +0900

    Add NewMain_Scene <스피어, 점프, 과일, 울타리, 동물>

---
+++ Chinese_Zodiac/Assets/Scenes/Scripts/Animal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+    int animal_state = 0;
+    public float speed = 20f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(animal_state==1)
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+            Vector3 direction = new Vector3(0, Input.GetAxis("Horizontal"), 0);
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Invoke("ChangeAnimalState", 0.5f);
+    }
+
+    void ChangeAnimalState()
+    {
+        animal_state = 1;
+    }
+}
--- Chinese_Zodiac/Assets/Scenes/Scripts/CatMove.cs
+++ Chinese_Zodiac/Assets/Scenes/Scripts/CatMove.cs
@@ -4,29 +4,50 @@ using UnityEngine;
 
 public class CatMove : MonoBehaviour {
 
-    public float speed = 5f;
-    public float jumpPower = 5.0f;
-   
+    public float speed = 10f;
+    public float jumpPower = 5f;
 
 
     CharacterController characterController;
+    Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
         characterController = GetComponent<CharacterController>();
-
+        rb = GetComponent<Rigidbody>();
 		
 	}
+
+
 	// Update is called once per frame
 	void Update () {
         transform.Translate(speed * Time.deltaTime, 0, 0);
         Vector3 direction = new Vector3(0, Input.GetAxis("Horizontal"), 0);
 
+
+       if(Input.GetKeyDown(KeyCode.UpArrow))
+        { 
+            rb.AddForce(new Vector3(0, 1, 0) * jumpPower, ForceMode.Impulse);
+        }
+
+        /*
         if (Input.GetButtonDown("Jump"))
         {
             GetComponent<Rigidbody>().velocity = new Vector3(0, jumpPower, 0);
-        }
+        }*/
 
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Apple")
+        {
+            Destroy(other.gameObject);
+        }
+        if (other.tag == "Obstacle")
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
 
     }
 }
